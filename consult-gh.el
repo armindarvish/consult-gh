3367fe8ba49a95ac8cd5830e793227957961b7b0
Armin Darvish <armindarvish@gmail.com>
2023-06-05 15:50:34 -0700

    fix crm issue

--- consult-gh.el
+++ consult-gh.el
@@ -782,7 +782,7 @@
 (defun consult-gh-orgs (&optional orgs)
 "Get a list of organizations from the user and provide their repos."
   (interactive
-   (let* ((crm-separator `,consult-gh-crm-separator)
+   (let* ((crm-separator consult-gh-crm-separator)
          (candidates (or (delete-dups (append consult-gh-default-orgs-list consult-gh--known-orgs-list)) (list))))
    (list (delete-dups (completing-read-multiple "Search GitHub Users/Organization: " candidates nil nil nil 'consult-gh--org-history nil t)))))
 
