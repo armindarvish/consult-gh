3b07180283d49d629a098511472c05f12f6924fb
Armin Darvish <armindarvish@gmail.com>
2025-01-11 02:16:55 -0800

    Add consult-gh-pkg.el

---
+++ consult-gh-pkg.el
@@ -0,0 +1,10 @@
+;; -*- no-byte-compile: t; lexical-binding: nil -*-
+
+(define-package "consult-gh" "2.2"
+  "Consulting GitHub Client."
+  '((emacs         "30.0")
+    (consult       "1.9")
+    (markdown-mode "2.6")
+    (ox-gfm        "1.0"))
+  :url "https://github.com/armindarvish/consult-gh"
+  :keywords '("convenience" "matching" "tools" "vc"))
