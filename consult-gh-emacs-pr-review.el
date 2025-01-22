2f1c2fd0f0b37845342fdd66f98c35e76127e710
Armin Darvish <armindarvish@gmail.com>
2025-01-22 04:36:38 -0800

    Fix issue with consult-split-style-character
    The previous commit was not tangled!

--- consult-gh-emacs-pr-review.el
+++ consult-gh-emacs-pr-review.el
@@ -6,7 +6,7 @@
 ;; Maintainer: Armin Darvish
 ;; Created: 2023
 ;; Version: 2.2
-;; Package-Requires: ((emacs "30.0") (consult "202501220") (pr-review "0.1") (consult-gh "2.2"))
+;; Package-Requires: ((emacs "30.0") (consult "20250121.1423") (pr-review "0.1") (consult-gh "2.2"))
 ;; Homepage: https://github.com/armindarvish/consult-gh
 ;; Keywords: matching, git, repositories, completion
 
